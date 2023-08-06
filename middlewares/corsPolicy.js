1207ba8aec2846a5db3bb6f3e1b3ff57c48cada7
Nikita Kovalev <mazoriksay.aod@gmail.com>
2023-08-06 23:19:21 +0300

    fix

--- middlewares/corsPolicy.js
+++ middlewares/corsPolicy.js
@@ -11,7 +11,7 @@ const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PATCH, POST, DELETE';
 module.exports.corsPolicy = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers['access-conrol-request-headers'];
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
